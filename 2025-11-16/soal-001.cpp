7a61cf32fc1129a5bc426b6868f837d379d5e191
mardovva <marddova.edu@gmail.com>
2025-11-16 23:10:15 +0700

    Latihan 2025-11-16

---
+++ 2025-11-16/soal-001.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right){
+    int temp[right - (left + 1)];
+
+    int i = left, j = mid + 1, k = 0;
+
+    while (i <= mid && j <= right){
+        if (arr[i] <= arr[j]){
+            temp[k++] = arr[i++];
+        } else{
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while (i <= mid){
+    temp[k++] = arr[i++];
+    }
+
+    while ( j <= right){
+    temp[k++] = arr[j++];
+    }
+
+    for (int i = left; i <= right; i++)
+    arr[i] = temp[i - left];
+}
+
+void mergeSort(int arr[], int left, int right){
+    if (left < right){
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    cin >> arr[i];
+
+    mergeSort(arr, 0, n - 1);
+    
+    for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
